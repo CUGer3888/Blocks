399aabca9cd4aa1962b4484023309b1e8786eff6
CUGer3888 <152231619+CUGer3888@users.noreply.github.com>
2024-08-29 11:15:53 +0800

    Add files via upload

--- 方块消除/方块消除/方块消除/FileName.cpp
+++ 方块消除/方块消除/方块消除/FileName.cpp
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 #include<graphics.h>
 #include<time.h>
@@ -7,157 +6,157 @@
 #include "tools.h"
 #include <mmsystem.h>//播放背景音乐的头文件
 #pragma comment(lib,"winmm.lib")//播放音乐需要的库文件
-#include <graphics.h>
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-#define WIDTH 800
-#define HEIGHT 600
-#define NUM_BLOCKS 10
-#define BLOCK_TYPES 4
-#define BLOCK_SIZE 40
-#define BLOCK_STEP 42
-#define OFFSET_X 45
-#define OFFSET_Y 39
-
-typedef struct {
+#define WIDTH 580
+#define HEIGHT 580
+/// <summary>
+/// 1.初始化窗口
+/// 2.随机生成方块
+/// 3.点击
+/// 4.判断是否可以消除
+/// 5.消除
+/// 6.方块下落
+/// 7.更新窗口
+/// </summary>
+IMAGE block_[4];
+IMAGE bg;
+int time_ = 0;
+int Step_size = 42;
+int off_x = 45;
+int off_y = 39;
+struct block {
+	int row;
+	int col;
 	int type;
-	int row, col;
 	int x, y;
 	bool isChecked;
-} Block;
+	int tmd;
 
-Block blocks[NUM_BLOCKS][NUM_BLOCKS];
-IMAGE bg, blockImages[BLOCK_TYPES];
-
-void loadResources() {
+};
+struct block blocks[10][10];
+void init() {
 	initgraph(WIDTH, HEIGHT);
 	loadimage(&bg, "res/bg_2.png");
 
 	char name[64];
-	for (int i = 0; i < BLOCK_TYPES; i++) {
+	for (int i = 0; i < 4; i++) {
 		sprintf_s(name, "res/%d.png", i + 1);
-		loadimage(&blockImages[i], name, BLOCK_SIZE, BLOCK_SIZE, true);
+		loadimage(&block_[i], name, 40, 40, true);
 	}
-}
 
-void initBlocks() {
 	srand(time(NULL));
-	for (int i = 0; i < NUM_BLOCKS; i++) {
-		for (int j = 0; j < NUM_BLOCKS; j++) {
-			blocks[i][j].type = 1 + rand() % BLOCK_TYPES;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			blocks[i][j].type = 1 + rand() % 4;
 			blocks[i][j].row = i;
 			blocks[i][j].col = j;
-			blocks[i][j].x = OFFSET_X + j * (BLOCK_STEP - 1);
-			blocks[i][j].y = OFFSET_Y + i * (BLOCK_STEP - 1);
+			blocks[i][j].x = 44 + j * (Step_size - 1);
+			blocks[i][j].y = 39 + i * (Step_size - 1);
 			blocks[i][j].isChecked = false;
+			blocks[i][j].tmd = 255;
 		}
 	}
 }
-
-void drawBackground() {
+void updateWindow() {
+	BeginBatchDraw();
 	putimage(0, 0, &bg);
-}
-
-void drawBlocks() {
-	for (int i = 0; i < NUM_BLOCKS; i++) {
-		for (int j = 0; j < NUM_BLOCKS; j++) {
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
 			if (blocks[i][j].type > 0 && !blocks[i][j].isChecked) {
-				putimagePNG(blocks[i][j].x, blocks[i][j].y, &blockImages[blocks[i][j].type - 1]);
+				putimagePNG(44 + blocks[i][j].col * (Step_size - 1), 39 + blocks[i][j].row * (Step_size - 1), &block_[blocks[i][j].type - 1]);
 			}
 		}
 	}
+	EndBatchDraw();
 }
-
-void moveBlocks() {
-	for (int i = 0; i < NUM_BLOCKS; i++) {
-		for (int j = 0; j < NUM_BLOCKS; j++) {
-			if (blocks[i][j].isChecked) {
-				continue;
-			}
-			int x = OFFSET_X + blocks[i][j].col * (BLOCK_STEP - 1);
-			int y = OFFSET_Y + blocks[i][j].row * (BLOCK_STEP - 1);
-
-			blocks[i][j].x = x;
-			blocks[i][j].y = y;
-		}
-	}
-}
-
-void checkForMatch(int x, int y, int type) {
-	if (blocks[x][y].isChecked || x < 0 || y < 0 || x >= NUM_BLOCKS || y >= NUM_BLOCKS) {
+void check(int x, int y, int type) {
+	// 检查边界
+	if (blocks[x][y].isChecked) return;
+	if (x < 0 || x >= 10 || y < 0 || y >= 10) {
 		return;
 	}
-	if (blocks[x][y].type == type) {
-		blocks[x][y].isChecked = true;
-		checkForMatch(x - 1, y, type);
-		checkForMatch(x + 1, y, type);
-		checkForMatch(x, y - 1, type);
-		checkForMatch(x, y + 1, type);
+	if (blocks[x][y].type == type && !blocks[x][y].isChecked) {
+		blocks[x][y].isChecked = true;  // 标记为已检查
+
+		// 递归地检查上、下、左、右四个方向的相邻点
+		check(x - 1, y, type);  // 上
+		check(x + 1, y, type);  // 下
+		check(x, y - 1, type);  // 左
+		check(x, y + 1, type);  // 右
 	}
 }
-void handleUserClick(ExMessage* msg) {
-	int col = (msg->x - OFFSET_X) / BLOCK_STEP;
-	int row = (msg->y - OFFSET_Y) / BLOCK_STEP;
-	checkForMatch(row, col, blocks[row][col].type);
+void userClick() {
+	ExMessage msg;
+	if (peekmessage(&msg) && msg.message == WM_LBUTTONDOWN) {
+		if (msg.x < off_x || msg.y < off_y) return;
+		int col = (msg.x - off_x) / Step_size;
+		int row = (msg.y - off_y) / Step_size;
+		if (col < 0 || row < 0 || col > 10 || row > 10) return;
+		printf("(%d,%d)\n", row, col);
+		check(row, col, blocks[row][col].type);
+	}
 }
-int* blockToIntPtr(Block* block) {
-	return (int*)block;
-}
-void swap(Block& a, Block& b) {
-	Block temp = a;
-	int row = a.row;
-	int row_1 = b.row;
-	a = b;
-	b = temp;
-	a.row = row;
-	b.row = row_1;
+void exchange(int row1, int col1, int row2, int col2) {
+	struct block tmp = blocks[row1][col1];
+	blocks[row1][col1] = blocks[row2][col2];
+	blocks[row2][col2] = tmp;
+	blocks[row1][col1].row = row1;
+	blocks[row1][col1].col = col1;
+	blocks[row2][col2].row = row2;
+	blocks[row2][col2].col = col2;
 }
 void updateGame() {
-	// Drop blocks down
-	for (int col = 0; col < NUM_BLOCKS; col++) {
-		for (int row = NUM_BLOCKS - 1; row >= 0; row--) {
-			if (blocks[row][col].isChecked) {
-				int newRow = row;
-				while (newRow > 0 && blocks[newRow - 1][col].type == 0) {
-					swap(blocks[newRow][col], blocks[newRow - 1][col]);
-					newRow--;
+	//降落
+	for (int i = 9; i >= 1; i--) {
+		for (int j = 1; j <= 9; j++) {
+			if (blocks[i][j].isChecked) {
+				printf("消除点(%d,%d)", i, j);
+			}
+		}
+	}
+	for (int i = 9; i >= 1; i--) {
+		for (int j = 1; j <= 9; j++) {
+			if (blocks[i][j].isChecked) {
+				for (int k = i - 1; k >= 1; k--) {
+					if (!blocks[k][j].isChecked) {
+						exchange(k, j, i, j);
+						break;
+					}
+
 				}
 			}
 		}
 	}
-	// Generate new blocks
-	for (int col = 0; col < NUM_BLOCKS; col++) {
-		for (int row = 0; row < NUM_BLOCKS; row++) {
-			if (blocks[row][col].type == 0) {
-				blocks[row][col].type = 1 + rand() % BLOCK_TYPES;
-				blocks[row][col].isChecked = false;
+	//生成新的方块进行更新
+	int n = 0;
+	for (int j = 1; j <= 9; j++) {
+
+		for (int i = 9; i >= 1; i--) {
+			if (blocks[i][j].isChecked) {
+				blocks[i][j].type = 1 + rand() % 4;
+				blocks[i][j].y = 39 + i * (Step_size - 1);
+				n++;
+				blocks[i][j].isChecked = false;
 			}
 		}
 	}
 }
-
-int main() {
-	loadResources();
-	initBlocks();
-
-	ExMessage msg;
-	while (true) {
-		if (peekmessage(&msg)) {
-			if (msg.message == WM_LBUTTONDOWN) {
-				handleUserClick(&msg);
-			}
+int main(void) {
+	init();
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			printf("%d,", blocks[i][j].type);
 		}
-		BeginBatchDraw();
-		drawBackground();
-		drawBlocks();
-		updateGame();
-		EndBatchDraw();
-		Sleep(10);
+		printf("\n");
 	}
+	updateWindow();
+	while (time_ < 100000) {
+		userClick();
 
+		updateGame();
+		updateWindow();
+		Sleep(10);
+		time_++;
+	}
 	system("pause");
 	return 0;
 }
